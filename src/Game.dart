4fc2d3e523d81e8bf332a6fa779088478f97ede8
Shannon -jj Behrens <jjinux@gmail.com>
2013-01-25 16:28:30 -0800

    Updated for libv2

--- src/Game.dart
+++ src/Game.dart
@@ -7,9 +7,10 @@
  * This code was inspired by Alexei Kourbatov (http://www.javascripter.net).
  */
 
-#library('game');
-
-#import('dart:html');
+library game;
+import 'dart:html';
+import 'dart:math';
+import 'dart:async';
 
 class Game {
 
@@ -25,7 +26,7 @@ class Game {
   static final FASTEST_SPEED = 60;
   static final SQUARE_WIDTH = 16;
   static final SQUARE_HEIGHT = 16;
-  
+
   // Keystroke processing
   static final INITIAL_DELAY = 200;
   static final REPEAT_DELAY = 20;
@@ -43,7 +44,7 @@ class Game {
   static final UP_IE = ' 38 56 53 104 101 ';
   static final DOWN_IE = ' 40 50 98 ';
   static final SPACE_IE = ' 32 ';
-  
+
   num curLevel = DEFAULT_LEVEL;
   num curX = 1;
   num curY = 1;
@@ -53,7 +54,7 @@ class Game {
   bool gamePaused = false;
   bool gameStarted = false;
   bool sayingBye = false;
-  num timerId = null;
+  Timer timer;
   num numLines = 0;
   num speed = SLOWEST_SPEED - FASTEST_SPEED * DEFAULT_LEVEL;
   List<ImageElement> squareImages;
@@ -66,20 +67,19 @@ class Game {
   List<num> dyPrime;
   List<List<num>> dxBank;
   List<List<num>> dyBank;
-  
+  Random random = new Random();
+
   // Keystroke processing
   bool isActiveLeft = false;
-  num timerLeft = null;
   bool isActiveRight = false;
-  num timerRight = null;
   bool isActiveUp = false;
-  num timerUp = null;
   bool isActiveDown = false;
-  num timerDown = null;
   bool isActiveSpace = false;
-  num timerSpace = null;
+  Timer timerLeft;
+  Timer timerRight;
+  Timer timerDown;
 
-  Game() {    
+  Game() {
     squareImages = [];
     board = [];
     xToErase = [0, 0, 0, 0];
@@ -93,7 +93,7 @@ class Game {
 
     for (num i = 0; i < NUM_IMAGES; i++) {
       ImageElement img = new Element.tag("img");
-      img.src = 'images/s' + i + '.png';
+      img.src = 'images/s${i}.png';
       squareImages.add(img);
     }
 
@@ -103,18 +103,18 @@ class Game {
         board[i].add(0);
       }
     }
-    
+
     document.on.keyDown.add(onKeyDown);
     document.on.keyUp.add(onKeyUp);
-    
-    SelectElement levelSelect = document.query("#level-select");
+
+    SelectElement levelSelect = query("#level-select");
     levelSelect.on.change.add((Event e) {
       onLevelSelectChange();
       levelSelect.blur();
     });
-    
-    ButtonElement startButton = document.query("#start-button");
-    ButtonElement pauseButton = document.query("#pause-button");
+
+    InputElement startButton = query("#start-button");
+    InputElement pauseButton = query("#pause-button");
     startButton.on.click.add((event) => start());
     pauseButton.on.click.add((event) => pause());
   }
@@ -123,7 +123,7 @@ class Game {
     drawBoard();
     resetGame();
   }
-  
+
   void start() {
     if (sayingBye) {
       window.history.back();
@@ -142,24 +142,24 @@ class Game {
     drawPiece();
     gameStarted = true;
     gamePaused = false;
-    InputElement numLinesField = document.query("#num-lines");
+    InputElement numLinesField = query("#num-lines");
     numLinesField.value = numLines.toString();
-    timerId = window.setTimeout(play, speed);
+    timer = new Timer(speed, (timer) => play());
   }
 
   void drawBoard() {
-    DivElement boardDiv = document.query("#board-div");
+    DivElement boardDiv = query("#board-div");
     PreElement pre = new Element.tag("pre");
     boardDiv.nodes.add(pre);
-    pre.classes.add("board");    
+    pre.classes.add("board");
     for (num i = 0; i < BOARD_HEIGHT; i++) {
       DivElement div = new Element.tag("div");
       pre.nodes.add(div);
       for (num j = 0; j < BOARD_WIDTH; j++) {
         ImageElement img = new Element.tag("img");
         div.nodes.add(img);
-        img.id = "s-" + i + "-" + j;
-        img.src = "images/s" + board[i][j].abs() + ".png";
+        img.id = "s-$i-$j";
+        img.src = "images/s${board[i][j].abs()}.png";
         img.width = SQUARE_WIDTH;
         img.height = SQUARE_HEIGHT;
       }
@@ -169,7 +169,7 @@ class Game {
       rightMargin.width = 1;
       rightMargin.height = SQUARE_HEIGHT;
     }
-    DivElement trailingDiv = new Element.tag("div");    
+    DivElement trailingDiv = new Element.tag("div");
     pre.nodes.add(trailingDiv);
     ImageElement trailingImg = new Element.tag("img");
     trailingDiv.nodes.add(trailingImg);
@@ -179,12 +179,12 @@ class Game {
     trailingImg.height = 1;
     boardDrawn = true;
   }
-  
+
   void resetGame() {
     for (num i = 0; i < BOARD_HEIGHT; i++) {
       for (num j = 0; j < BOARD_WIDTH; j++) {
         board[i][j] = 0;
-        ImageElement img = document.query("#s-" + i + "-" + j);
+        ImageElement img = query("#s-$i-$j");
         img.src = 'images/s0.png';
       }
     }
@@ -193,11 +193,11 @@ class Game {
     numLines = 0;
     curLevel = 1;
     skyline = BOARD_HEIGHT - 1;
-    InputElement numLinesField = document.query("#num-lines");
+    InputElement numLinesField = query("#num-lines");
     numLinesField.value = numLines.toString();
-    SelectElement levelSelect = document.query("#level-select");
+    SelectElement levelSelect = query("#level-select");
     levelSelect.selectedIndex = 0;
-    
+
     // I shouldn't have to call this manually, but I do.
     // See: http://code.google.com/p/dart/issues/detail?id=2325&thanks=2325&ts=1332879888
     onLevelSelectChange();
@@ -205,12 +205,12 @@ class Game {
 
   void play() {
     if (moveDown()) {
-      timerId = window.setTimeout(play, speed);
+      timer = new Timer(speed, (timer) => play());
     } else {
       fillMatrix();
       removeLines();
       if (skyline > 0 && getPiece()) {
-        timerId = window.setTimeout(play, speed);
+        timer = new Timer(speed, (timer) => play());
       } else {
         isActiveLeft = false;
         isActiveUp = false;
@@ -228,7 +228,7 @@ class Game {
         resume();
         return;
       }
-      window.clearTimeout(timerId);
+      timer.cancel();
       gamePaused = true;
     }
   }
@@ -236,8 +236,8 @@ class Game {
   void onKeyDown(KeyboardEvent event) {
     // I'm positive there are more modern ways to do keyboard event handling.
     String keyNN, keyIE;
-    keyNN = ' ' + event.keyCode + ' ';
-    keyIE = ' ' + event.keyCode + ' ';
+    keyNN = ' ${event.keyCode} ';
+    keyIE = ' ${event.keyCode} ';
 
     // Only preventDefault if we can actually handle the keyDown event.  If we
     // capture all keyDown events, we break things like using ^r to reload the page.
@@ -251,14 +251,14 @@ class Game {
         isActiveLeft = true;
         isActiveRight = false;
         moveLeft();
-        timerLeft = window.setTimeout(slideLeft, INITIAL_DELAY);
+        timerLeft = new Timer(INITIAL_DELAY, (timer) => slideLeft());
       }
     } else if (RIGHT_NN.indexOf(keyNN) != -1 || RIGHT_IE.indexOf(keyIE) != -1) {
       if (!isActiveRight) {
         isActiveRight = true;
         isActiveLeft = false;
         moveRight();
-        timerRight = window.setTimeout(slideRight, INITIAL_DELAY);
+        timerRight = new Timer(INITIAL_DELAY, (timer) => slideRight());
       }
     } else if (UP_NN.indexOf(keyNN) != -1 || UP_IE.indexOf(keyIE) != -1) {
       if (!isActiveUp) {
@@ -277,34 +277,32 @@ class Game {
         isActiveDown = true;
         isActiveUp = false;
         moveDown();
-        timerDown = window.setTimeout(slideDown, INITIAL_DELAY);
+        timerDown = new Timer(INITIAL_DELAY, (timer) => slideDown());
       }
     } else {
       return;
     }
-    event.preventDefault();  
+    event.preventDefault();
   }
-  
+
   void onKeyUp(KeyboardEvent event) {
     // See comments in onKeyDown.
     var keyNN, keyIE;
-    keyNN = ' ' + event.keyCode + ' ';
-    keyIE = ' ' + event.keyCode + ' ';
+    keyNN = ' ${event.keyCode} ';
+    keyIE = ' ${event.keyCode} ';
     if (LEFT_NN.indexOf(keyNN) != -1 || LEFT_IE.indexOf(keyIE) != -1) {
       isActiveLeft = false;
-      window.clearTimeout(timerLeft);
+      timerLeft.cancel();
     } else if (RIGHT_NN.indexOf(keyNN) != -1 || RIGHT_IE.indexOf(keyIE) != -1) {
       isActiveRight = false;
-      window.clearTimeout(timerRight);
+      timerRight.cancel();
     } else if (UP_NN.indexOf(keyNN) != -1 || UP_IE.indexOf(keyIE) != -1) {
       isActiveUp = false;
-      window.clearTimeout(timerUp);
     } else if (DOWN_NN.indexOf(keyNN) != -1 || DOWN_IE.indexOf(keyIE) != -1) {
       isActiveDown = false;
-      window.clearTimeout(timerDown);
+      timerDown.cancel();
     } else if (SPACE_NN.indexOf(keyNN) != -1 || SPACE_IE.indexOf(keyIE) != -1) {
       isActiveSpace = false;
-      window.clearTimeout(timerSpace);
     } else {
       return;
     }
@@ -340,29 +338,29 @@ class Game {
         for (k = i; k >= skyline; k--) {
           for (j = 0; j < BOARD_WIDTH; j++) {
             board[k][j] = board[k - 1][j];
-            ImageElement img = document.query("#s-" + k + "-" + j);
+            ImageElement img = query("#s-$k-$j");
             img.src = squareImages[board[k][j]].src;
           }
         }
         for (j = 0; j < BOARD_WIDTH; j++) {
           board[0][j] = 0;
-          ImageElement img = document.query("#s-0-" + j);
+          ImageElement img = query("#s-0-$j");
           img.src = squareImages[0].src;
         }
         numLines++;
         skyline++;
-        InputElement numLinesField = document.query("#num-lines");
+        InputElement numLinesField = query("#num-lines");
         numLinesField.value = numLines.toString();
         if (numLines % ROWS_PER_LEVEL == 0 && curLevel < MAX_LEVEL) {
           curLevel++;
         }
         speed = SLOWEST_SPEED - FASTEST_SPEED * curLevel;
-        SelectElement levelSelect = document.query("#level-select");
+        SelectElement levelSelect = query("#level-select");
         levelSelect.selectedIndex = curLevel - 1;
       }
     }
   }
-  
+
   bool pieceFits(x, y) {
     num k, theX, theY;
     for (k = 0; k < NUM_SQUARES; k++) {
@@ -392,7 +390,7 @@ class Game {
       }
     }
   }
-  
+
   void drawPiece() {
     num k, x, y;
     if (boardDrawn) {
@@ -400,14 +398,14 @@ class Game {
         x = curX + dx[k];
         y = curY + dy[k];
         if (0 <= y && y < BOARD_HEIGHT && 0 <= x && x < BOARD_WIDTH && board[y][x] != -curPiece) {
-          ImageElement img = document.query("#s-" + y + "-" + x);
+          ImageElement img = query("#s-$y-$x");
           img.src = squareImages[curPiece].src;
           board[y][x] = -curPiece;
         }
         x = xToErase[k];
         y = yToErase[k];
         if (board[y][x] == 0) {
-          ImageElement img = document.query("#s-" + y + "-" + x);
+          ImageElement img = query("#s-$y-$x");
           img.src = squareImages[0].src;
         }
       }
@@ -457,7 +455,7 @@ class Game {
 
   bool getPiece() {
     num k;
-    curPiece = 1 + (NUM_TYPES * Math.random()).floor();
+    curPiece = 1 + random.nextInt(NUM_TYPES);
     curX = 5;
     curY = 0;
     for (k = 0; k < NUM_SQUARES; k++) {
@@ -483,9 +481,9 @@ class Game {
   }
 
   void onLevelSelectChange() {
-    SelectElement levelSelect = document.query("#level-select");
+    SelectElement levelSelect = query("#level-select");
     OptionElement selectedOption = levelSelect.options[levelSelect.selectedIndex];
-    curLevel = Math.parseInt(selectedOption.value);
+    curLevel = int.parse(selectedOption.value);
     speed = SLOWEST_SPEED - FASTEST_SPEED * curLevel;
   }
 
@@ -504,7 +502,7 @@ class Game {
       drawPiece();
     }
   }
-  
+
   void fall() {
     num k;
     for (k = 0; k < NUM_SQUARES; k++) {
@@ -514,33 +512,33 @@ class Game {
     if (!pieceFits(curX, curY + 1)) {
       return;
     }
-    window.clearTimeout(timerId);
+    timer.cancel();
     erasePiece();
     while (pieceFits(curX, curY + 1)) {
       curY++;
     }
     drawPiece();
-    timerId = window.setTimeout(play, speed);
+    timer = new Timer(speed, (timer) => play());
   }
 
   void slideLeft() {
     if (isActiveLeft) {
       moveLeft();
-      timerLeft = window.setTimeout(slideLeft, REPEAT_DELAY);
+      timerLeft = new Timer(REPEAT_DELAY, (timer) => slideLeft());
     }
   }
 
   void slideRight() {
     if (isActiveRight) {
       moveRight();
-      timerRight = window.setTimeout(slideRight, REPEAT_DELAY);
+      timerRight = new Timer(REPEAT_DELAY, (timer) => slideRight());
     }
   }
 
   void slideDown() {
     if (isActiveDown) {
       moveDown();
-      timerDown = window.setTimeout(slideDown, REPEAT_DELAY);
+      timerDown = new Timer(REPEAT_DELAY, (timer) => slideDown());
     }
   }
 
@@ -549,7 +547,7 @@ class Game {
     p.text = message;
     document.body.nodes.add(p);
   }
-  
+
 }
 
 void main() {
